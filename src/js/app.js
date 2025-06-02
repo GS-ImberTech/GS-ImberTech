352231b1f31ddc4cbe3943ceadac2bf38d3b5ce1
Vitor Fernandes dos Santos <vitorfernandes11122006@gmail.com>
2025-06-02 13:31:00 -0300

    inserção dos slides nos containers

--- src/js/app.js
+++ src/js/app.js
@@ -15,4 +15,43 @@ menuClick.addEventListener('click', function (event){
     show.showdisplay = true
     }
 
+<<<<<<< Updated upstream
 })
+=======
+})
+
+// SLIDER
+
+let slideAtual = 0;
+const slides = document.querySelectorAll(".slide");
+const totalSlides = slides.length;
+
+const btnNext = document.getElementById("btnNext");
+const btnPrev = document.getElementById("btnPrev");
+
+btnNext.addEventListener('click', function() {
+    nextSlide();
+});
+
+btnPrev.addEventListener('click', function() {
+    prevSlide();
+});
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[index].classList.add('active');
+}
+
+function nextSlide() {
+    slideAtual = (slideAtual + 1) % totalSlides;
+    showSlide(slideAtual);
+}
+
+function prevSlide() {
+    slideAtual = (slideAtual - 1 + totalSlides) % totalSlides;
+    showSlide(slideAtual);
+}
+
+// Automático
+setInterval(nextSlide, 7000)
+>>>>>>> Stashed changes
