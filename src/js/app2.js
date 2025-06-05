763841084af013c2f374e84464107465deb3b67d
Vitor Fernandes dos Santos <vitorfernandes11122006@gmail.com>
2025-06-05 00:44:31 -0300

    Criação de JS escpecifíco para a página

--- src/js/app2.js
+++ src/js/app2.js
@@ -1,17 +1,17 @@
 const menuClick = document.getElementById("menubtn");
-var show = {showdisplay: true}
+var show = { showdisplay: true }
 
 // Menu hamburguer
-menuClick.addEventListener('click', function (event){
+menuClick.addEventListener('click', function (event) {
 
     event.preventDefault();
-    if (show.showdisplay == true){
-    menulist.style.display = 'flex';
-    show.showdisplay = false
+    if (show.showdisplay == true) {
+        menulist.style.display = 'flex';
+        show.showdisplay = false
     }
-    else{
-    menulist.style.display = 'none';
-    show.showdisplay = true
+    else {
+        menulist.style.display = 'none';
+        show.showdisplay = true
     }
 
 })
@@ -37,7 +37,7 @@ function changeBackgroundWhite() {
 
     document.getElementById(menubtn.id).style.backgroundColor = "#083d83";
     document.getElementById(menulist.id).style.backgroundColor = "#14488b";
-    
+
 
     document.getElementById(h1inicio.id).style.color = "black";
 
@@ -49,9 +49,9 @@ function changeBackgroundWhite() {
         document.getElementById(secoes[i].id).style.backgroundColor = 'white';
     }
     for (let i = 0; i < secoes.length; i++) {
-      document.getElementById(secoes[i].id).style.boxShadow = 'rgba(0, 0, 0, 0.3) 0px -10px 20px,  rgba(0, 0, 0, 0.22) 0px 19px 38px,rgba(0, 0, 0, 0.22) 0px 15px 12px';
-    }  
-    for (let i=0; i < gradientes.length; i++){
+        document.getElementById(secoes[i].id).style.boxShadow = 'rgba(0, 0, 0, 0.3) 0px -10px 20px,  rgba(0, 0, 0, 0.22) 0px 19px 38px,rgba(0, 0, 0, 0.22) 0px 15px 12px';
+    }
+    for (let i = 0; i < gradientes.length; i++) {
         document.getElementById(gradientes[i].id).style.backgroundImage = 'radial-gradient(rgb(48, 97, 164), rgb(5, 51, 113))';
     }
 
@@ -71,15 +71,15 @@ function changeBackgroundBlack() {
     document.getElementById(gradientbtn0.id).style.color = "white";
     document.getElementById(gradientbtn0.id).style.borderColor = "black";
     document.getElementById(gradientbtn0.id).style.backgroundColor = "rgb(4, 12, 22)";
-   
+
 
     for (let i = 0; i < secoes.length; i++) {
         document.getElementById(secoes[i].id).style.backgroundColor = 'rgb(0, 15, 34)';
     }
     for (let i = 0; i < secoes.length; i++) {
-      document.getElementById(secoes[i].id).style.boxShadow = 'rgba(3, 3, 3, 0.3) 0px 19px 38px, rgba(3, 3, 3, 0.22) 0px 15px 12px';
-    }  
-    for (let i=0; i < gradientes.length; i++){
+        document.getElementById(secoes[i].id).style.boxShadow = 'rgba(3, 3, 3, 0.3) 0px 19px 38px, rgba(3, 3, 3, 0.22) 0px 15px 12px';
+    }
+    for (let i = 0; i < gradientes.length; i++) {
         document.getElementById(gradientes[i].id).style.backgroundImage = 'radial-gradient(rgb(8, 40, 83),rgb(0, 25, 58)';
     }
 }
@@ -92,20 +92,121 @@ function changeBackgroundGrey() {
 
     document.getElementById(menubtn.id).style.backgroundColor = "rgb(0, 0, 0)";
     document.getElementById(menulist.id).style.backgroundColor = "rgb(0, 0, 0)";
-    
+
     document.getElementById(h1inicio.id).style.color = "black";
 
     document.getElementById(gradientbtn0.id).style.color = "black";
     document.getElementById(gradientbtn0.id).style.borderColor = "black";
     document.getElementById(gradientbtn0.id).style.backgroundColor = "white";
-    
+
 
     for (let i = 0; i < secoes.length; i++) {
         document.getElementById(secoes[i].id).style.backgroundColor = 'rgb(0, 0, 0)';
         document.getElementById(secoes[i].id).style.boxShadow = 'none';
     }
-    for (let i=0; i < gradientes.length; i++){
+    for (let i = 0; i < gradientes.length; i++) {
         document.getElementById(gradientes[i].id).style.backgroundImage = 'radial-gradient(rgb(0, 0, 0),rgb(0, 0, 0)';
     }
+
 }
+const dados = [
+    { id: '08225300.2', categoria: 'alto', cepid: '08225300' },
+    { id: '08265601', categoria: 'alto', cepid: '08265601' },
+    { id: '08225310', categoria: 'alto', cepid: '08225310' },
+    { id: '08225310.2', categoria: 'alto', cepid: '08225310' },
+    { id: '08143977', categoria: 'media', cepid: '08143977' },
+    { id: '08452110', categoria: 'media', cepid: '08452110' },
+    { id: '08452129', categoria: 'media', cepid: '08452129' },
+    { id: '08452129.2', categoria: 'media', cepid: '08452129' },
+    { id: '08452123', categoria: 'baixa', cepid: '08452123' },
+    { id: '08225300', categoria: 'baixa', cepid: '08225300' },
+    { id: '08225310.3', categoria: 'baixa', cepid: '08225310' }
+];
+
+
+const filtroClick = document.getElementById('btnfiltro');
+const apagarClick = document.getElementById('btnapagar');
+
+apagarClick.addEventListener('click', function(event){
+    event.preventDefault();
+    document.querySelector('form').reset();
+
+    document.getElementById('nenhumdisp').style.display = 'none'; 
+    for (let i = 0; i < dados.length; i++) {
+        document.getElementById(dados[i].id).style.display = 'flex';
+    }
+})
+
+filtroClick.addEventListener('click', function (event) {
+
+    const cat = document.getElementById('categoria').value;
+    const cep = document.getElementById('cep').value;
+    event.preventDefault();
+
+    if (cep.length != 8 && cep != ''){
+        alert( `O CEP "${cep}" não é válido\nPor gentileza, insira um CEP composto por 8 digítos numéricos`)
+    }
+    if(cep.length == 8){
+        for (let i = 0; i < dados.length; i++) {
+            document.getElementById(dados[i].id).style.display = 'flex';}
+
+        for (let i = 0; i < dados.length; i++) {
+            if(dados[i].cepid != cep){
+             document.getElementById(dados[i].id).style.display = 'none';}}
+    }
+    if (cat == 'cat1') {
+        for (let i = 0; i < dados.length; i++) {
+            if (dados[i].categoria != 'baixa') {
+                document.getElementById(dados[i].id).style.display = 'none';
+            }
+        }
+    }
+    if (cat == 'cat2') {
+        for (let i = 0; i < dados.length; i++) {
+            if (dados[i].categoria != 'media') {
+                document.getElementById(dados[i].id).style.display = 'none';
+            }
+        }
+    }
+    if (cat == 'cat3') {
+        for (let i = 0; i < dados.length; i++) {
+            if (dados[i].categoria != 'alto') {
+                document.getElementById(dados[i].id).style.display = 'none';
+            }
+        }
+    }
+    let total = 0;
+    for (let i = 0; i < dados.length; i++) {
+        if (document.getElementById(dados[i].id).style.display == 'none') {
+            total++;
+        }
+    }
+    if (total == dados.length) {
+        document.getElementById('nenhumdisp').style.display = 'flex'; 
+    } else {
+        document.getElementById('nenhumdisp').style.display = 'none'; 
+    }
+
+})
+
+var ordenar = {inverte: true} 
+
+precoClick = document.getElementById('btnordem');
+
+precoClick.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const btnordem = document.getElementById('btnordem');
+
+    if (ordenar.inverte === true) {
+        document.getElementById('containerdados').style.flexDirection = 'column-reverse';
+        btnordem.innerHTML = "Do mais limpo para o mais entupido ";
+        ordenar.inverte = false;
+    } 
+    else {
+        document.getElementById('containerdados').style.flexDirection = 'column';
+        btnordem.innerHTML = "Do mais entupido para o mais limpo";
+        ordenar.inverte = true;
+    }
+});
 
